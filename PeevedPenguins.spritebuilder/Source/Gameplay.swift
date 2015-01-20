622efc016df2694bc2d927d18313d68cecad22eb
Dion Larson <me@dionlarson.com>
2015-01-20 15:41:47 -0800

    Catapult pullback.

--- PeevedPenguins.spritebuilder/Source/Gameplay.swift
+++ PeevedPenguins.spritebuilder/Source/Gameplay.swift
@@ -14,16 +14,48 @@ class Gameplay: CCNode {
   var contentNode: CCNode!
   var catapultArm: CCNode!
   var levelNode: CCNode!
+  var pullbackNode: CCNode!
+  var mouseJointNode: CCNode!
+  var mouseJoint: CCPhysicsJoint?
   
   func didLoadFromCCB() {
     userInteractionEnabled = true
     
     let level: CCNode = CCBReader.load("Levels/Level1")
     levelNode.addChild(level)
+    
+    pullbackNode.physicsBody.collisionMask = []
+    mouseJointNode.physicsBody.collisionMask = []
+    physicsNode.debugDraw = true
   }
   
   override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
-    launchPenguin()
+    var touchLocation = touch.locationInNode(contentNode)
+    
+    if CGRectContainsPoint(catapultArm.boundingBox(), touchLocation) {
+      mouseJointNode.position = touchLocation
+      mouseJoint = CCPhysicsJoint.connectedSpringJointWithBodyA(mouseJointNode.physicsBody, bodyB: catapultArm.physicsBody, anchorA: CGPointZero, anchorB: CGPoint(x: 34, y: 138), restLength: 0, stiffness: 3000, damping: 150)
+    }
+  }
+  
+  override func touchMoved(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+    var touchLocation = touch.locationInNode(contentNode)
+    mouseJointNode.position = touchLocation
+  }
+  
+  override func touchEnded(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+    releaseCatapult()
+  }
+  
+  override func touchCancelled(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+    releaseCatapult()
+  }
+  
+  func releaseCatapult() {
+    if let joint = mouseJoint {
+      joint.invalidate()
+      mouseJoint = nil
+    }
   }
   
   func launchPenguin() {
